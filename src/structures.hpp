d33f955fd35b4f9b2cc99f66a3793a948f3b8b87
Thomas Minier <tminier01@gmail.com>
2016-01-18 17:49:12 +0100

    add destructors

--- src/structures.hpp
+++ src/structures.hpp
@@ -20,22 +20,26 @@ typedef struct _Node { } Node;
 typedef struct _Conc : public Node {
 	Node * left;
 	Node * right;
+	~_Conc() { delete left, right; }
 } Conc;
 
 // Noeud représentant une union
 typedef struct _Union : public Node {
 	Node * left;
 	Node * right;
+	~_Union() { delete left, right; }
 } Union;
 
 // Noeud représentant une opération [x]
 typedef struct _Star : public Node {
 	Node * Star_e;
+	~_Star() { delete Star_e; }
 } Star;
 
 // Noeud représentant une opération (/x/)
 typedef struct _UN : public Node {
 	Node * UN_e;
+	~_UN() { delete UN_e; }
 } UN;
 
 // Noeud représentant un terminal ou non-terminal
@@ -43,6 +47,7 @@ typedef struct _Atom : public Node {
 	int code;
 	int action;
 	ATOMTYPE AType;
+	~_Atom() { }
 } Atom;
 
 // Tableau de la grammaire des grammaires
